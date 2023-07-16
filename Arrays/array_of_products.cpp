829ac33716f288fbc8f7f2057850452dfc8bad41
Siva Prasad <104807040+Siva1427-tech@users.noreply.github.com>
2023-07-16 14:34:46 +0530

    Update array_of_products.cpp

--- Arrays/array_of_products.cpp
+++ Arrays/array_of_products.cpp
@@ -1,6 +1,6 @@
 /*
 	Given an array of integers A, find and return the product array of the same size where the ith
-	element of the product array will be equal to the product of all the elements divided by the ith
+	element of the product array will be equal to  product of all the elements divided by the ith
 	element of the array.
 
 	Note: It is always possible to form the product array with integer (32 bit) values.
